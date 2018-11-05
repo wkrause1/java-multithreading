f58ef96bde17e0331958ebd1f171e9f6b4586b38
Will Krause <wkrause1@radford.edu>
2018-11-05 09:39:22 -0500

    added lock feature, doesn't work correctly yet

--- ThreadTest.java
+++ ThreadTest.java
@@ -1,5 +1,8 @@
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ThreadTest implements Runnable {
     private String greeting;
+    private ReentrantLock lock;
     public ThreadTest(String s) {
         this.greeting = s;
     }
@@ -7,10 +10,15 @@ public class ThreadTest implements Runnable {
     public void run() {
         for (int i=1; i<11; i++) {
             System.out.println(greeting);
+            lock = new ReentrantLock();
+            lock.lock();
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
+            }
+            finally{
+                lock.unlock();
             }
         }
     }
